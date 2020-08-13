1d673a34a7ed4836783aed6ee723149b35378ef9
Igor Spasić <igor.spasic@gmail.com>
2020-08-13 06:54:23 +0200

    New benchmark.

--- src/main/java/com/oblac/jmh/lang/NewBenchmark.java
+++ src/main/java/com/oblac/jmh/lang/NewBenchmark.java
@@ -14,9 +14,10 @@ import java.util.concurrent.TimeUnit;
 
 /**
  Benchmark                Mode  Cnt  Score   Error  Units
- NewBenchmark.allocArray  avgt   20  3.261 ± 0.026  ns/op
- NewBenchmark.createFoo   avgt   20  2.927 ± 0.065  ns/op
- */
+ NewBenchmark.allocArray    avgt   20  3.282 ± 0.060  ns/op
+ NewBenchmark.createBigFoo  avgt   20  4.004 ± 0.041  ns/op
+ NewBenchmark.createFoo     avgt   20  2.896 ± 0.045  ns/op
+*/
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
@@ -35,9 +36,69 @@ public class NewBenchmark {
 		}
 	}
 
+	public static class BigFoo {
+		private final int value;
+		private int a;
+		private String b;
+		private Double c;
+		private Foo d;
+		private Long e;
+		public BigFoo(int value) {
+			this.value = value;
+		}
+		public int getValue() {
+			return value;
+		}
+
+		public int getA() {
+			return a;
+		}
+
+		public void setA(int a) {
+			this.a = a;
+		}
+
+		public String getB() {
+			return b;
+		}
+
+		public void setB(String b) {
+			this.b = b;
+		}
+
+		public Double getC() {
+			return c;
+		}
+
+		public void setC(Double c) {
+			this.c = c;
+		}
+
+		public Foo getD() {
+			return d;
+		}
+
+		public void setD(Foo d) {
+			this.d = d;
+		}
+
+		public Long getE() {
+			return e;
+		}
+
+		public void setE(Long e) {
+			this.e = e;
+		}
+	}
+
 	@Benchmark
 	public Foo createFoo() {
 		return new Foo(1);
+	}
+
+	@Benchmark
+	public BigFoo createBigFoo() {
+		return new BigFoo(1);
 	}
 
 	@Benchmark
